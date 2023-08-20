2daf3a3202e592e4e321b299bed741df8aa2d937
Yash Dabke <112268819+yashdabke@users.noreply.github.com>
2023-08-21 00:06:30 +0530

    Create counter_up.sv

---
+++ counter_up.sv
@@ -0,0 +1,10 @@
+// simple 4-bit up counter
+
+module counter_up (input logic d, clk, rst, output logic [3:0] counter);
+	always @(posedge clk or posedge rst) begin
+		if(rst)
+			counter <= 4'b0;
+		else 
+			counter <= counter + 4'b1;
+	end
+endmodule
